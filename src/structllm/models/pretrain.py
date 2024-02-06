cc28b777ed6b0a5c4cec68d60d529893d3ce63ac
n0w0f <pvt.nawaf@gmail.com>
2024-02-06 14:17:55 +0100

    fix: cleanup

--- src/structllm/models/pretrain.py
+++ src/structllm/models/pretrain.py
@@ -1,6 +1,4 @@
 from typing import Any, List, Dict, Union
-
-
 import wandb
 from omegaconf import DictConfig
 
@@ -8,38 +6,74 @@ from transformers import DataCollatorForLanguageModeling
 from transformers import AutoModelForMaskedLM, AutoConfig
 from transformers import Trainer, TrainingArguments
 from datasets import load_dataset
+import pandas as pd
+from datasets import Dataset, DatasetDict
 from transformers import TrainerCallback
-from datasets import load_dataset
+
 
 from torch import nn
 from torch.nn.parallel import DistributedDataParallel
 from transformers import EarlyStoppingCallback
 from structllm.models.utils import CustomWandbCallback_Pretrain, TokenizerMixin
-
+from functools import partial
 
 
 class PretrainModel(TokenizerMixin):
     """Class to perform pretraining of a language model."""
     def __init__(self, cfg: DictConfig, local_rank=None):
+
         super().__init__(cfg.tokenizer)
         self.cfg = cfg.model.pretrain
         self.local_rank = local_rank
         self.context_length: int = self.cfg.context_length
+        self.callbacks = self.cfg.callbacks
         self.model_name_or_path: str = self.cfg.model_name_or_path
 
-        train_dataset = load_dataset("csv", data_files=self.cfg.path.traindata)
-        eval_dataset = load_dataset("csv", data_files=self.cfg.path.evaldata)
+        self.tokenized_dataset = self._prepare_datasets
 
-        self.tokenized_train_datasets = train_dataset.map(self._tokenize_pad_and_truncate, batched=True)
-        self.tokenized_eval_datasets = eval_dataset.map(self._tokenize_pad_and_truncate, batched=True)
+    @property
+    def _prepare_datasets(self) -> DatasetDict:
+        """
+        Prepare training and validation datasets.
 
-    def _wandb_callbacks(self) -> List[TrainerCallback]:
-        """Returns a list of callbacks for logging."""
-        return [CustomWandbCallback_Pretrain()]
+        Args:
+            train_df (pd.DataFrame): DataFrame containing training data.
 
-    def _tokenize_pad_and_truncate(self, texts: Dict[str, Any]) -> Dict[str, Any]:
-        """Tokenizes, pads, and truncates input texts."""
-        return self._wrapped_tokenizer(texts["slices"], truncation=True, padding="max_length", max_length=self.context_length)
+        Returns:
+            DatasetDict: Dictionary containing training and validation datasets.
+        """
+
+        train_df = pd.read_csv(self.cfg.path.traindata)
+        val_df = pd.read_csv(self.cfg.path.evaldata)
+
+        datasets = DatasetDict({
+            'train': Dataset.from_pandas(train_df),
+            'test': Dataset.from_pandas(val_df)
+        })
+
+        # Tokenize, pad, and truncate the datasets
+        tokenized_datasets = {
+            k: v.map(partial(self._tokenize_pad_and_truncate, context_length=self.context_length), batched=True)
+            for k, v in datasets.items()
+        }
+
+        return tokenized_datasets
+    
+    def _callbacks(self) -> List[TrainerCallback]:
+        """Returns a list of callbacks for early stopping, and custom logging."""
+        callbacks = []
+
+        if self.callbacks.early_stopping:
+            callbacks.append(EarlyStoppingCallback(
+                early_stopping_patience=self.callbacks.early_stopping_patience,
+                early_stopping_threshold=self.callbacks.early_stopping_threshold
+            ))
+
+        if self.callbacks.custom_logger:
+            callbacks.append(CustomWandbCallback_Pretrain())
+
+        return callbacks
+
 
     def pretrain_mlm(self) -> None:
         """Performs MLM pretraining of the language model."""
@@ -53,8 +87,7 @@ class PretrainModel(TokenizerMixin):
             mlm_probability=config_mlm.mlm_probability
         )
 
-        callbacks = self._wandb_callbacks()
-        early_stopping = EarlyStoppingCallback(early_stopping_patience=6)
+        callbacks = self._callbacks()
 
         config = AutoConfig.from_pretrained(
             self.model_name_or_path,
@@ -76,10 +109,10 @@ class PretrainModel(TokenizerMixin):
         trainer = Trainer(
             model=model,
             data_collator=data_collator,
-            train_dataset=self.tokenized_train_datasets['train'],
-            eval_dataset=self.tokenized_eval_datasets['train'],
+            train_dataset=self.tokenized_dataset['train'],
+            eval_dataset=self.tokenized_dataset['test'],
             args=training_args,
-            callbacks= [early_stopping] #+ callbacks
+            callbacks= callbacks
         )
 
         wandb.log({"config_details": str(config)}) 
