35aa9cd602f9fecde462a771d3af019dbaac17a2
n0w0f <pvt.nawaf@gmail.com>
2024-04-09 15:52:50 +0200

    new templates

--- src/structllm/dataprep/matbench_prepare_data.py
+++ src/structllm/dataprep/matbench_prepare_data.py
@@ -58,7 +58,8 @@ def process_entry_train_matbench(entry: dict, timeout: int) -> dict:
     try:
         signal.alarm(timeout)  # Start the timer
         #text_reps = TextRep.from_input(entry["structure"]).get_all_text_reps()
-        text_reps = TextRep.from_input(entry["structure"],permute_atoms=False).get_requested_text_reps(["zmatrix"])
+        text_reps = TextRep.from_input(entry["structure"],permute_atoms=False).get_requested_text_reps(
+            ["slice","composition","cif_symmetrized","cif_p1", "crystal_llm_rep","atoms", "atoms_params","zmatrix","wycoff_rep"])
         text_reps['mbid'] = entry["mbid"]
         text_reps['labels'] = entry["labels"]
         signal.alarm(0)  # Reset the timer
@@ -84,7 +85,8 @@ def process_entry_test_matbench(entry: dict, timeout: int) -> dict:
     try:
         signal.alarm(timeout)  # Start the timer
         # text_reps = TextRep.from_input(entry["structure"],permute_atoms=True,seed_for_transformations=7).get_requested_text_reps(["crystal_llm_rep", "cif_p1", "slice", "atoms", "atoms_params"])
-        text_reps = TextRep.from_input(entry["structure"],permute_atoms=False).get_requested_text_reps(["zmatrix"])
+        text_reps = TextRep.from_input(entry["structure"],permute_atoms=False).get_requested_text_reps(
+            ["slice","composition","cif_symmetrized","cif_p1", "crystal_llm_rep","atoms", "atoms_params","zmatrix","wycoff_rep"])
         text_reps['mbid'] = entry["mbid"]
         signal.alarm(0)  # Reset the timer
         return text_reps
