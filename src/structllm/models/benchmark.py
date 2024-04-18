9ceb03e8d2629f253f489b408953a342d7ed1c30
n0w0f <pvt.nawaf@gmail.com>
2024-04-19 01:24:46 +0200

    chore: add templates from recent exp

--- src/structllm/models/benchmark.py
+++ src/structllm/models/benchmark.py
@@ -76,3 +76,44 @@ class Matbenchmark:
 
         file_name = os.path.join(self.benchmark_save_path, f"{self.representation}_{self.benchmark}.json.gz")
         benchmark.to_file(file_name)
+
+    def run_qmof(self, local_rank=None) -> None:
+        
+        for i, (exp_name, test_name, train_data_path, test_data_path) in enumerate(
+            zip(self.exp_names, self.test_exp_names, self.train_data, self.test_data)
+        ):
+            print(f"Running training on {train_data_path}, and testing on {test_data_path}")
+            wandb.init(
+                config=dict(self.task_cfg.model.finetune),
+                project=self.task_cfg.model.logging.wandb_project, name=exp_name)
+
+            exp_cfg = self.task_cfg.copy()
+            exp_cfg.model.finetune.exp_name = exp_name
+            exp_cfg.model.finetune.path.finetune_traindata = train_data_path
+
+
+            finetuner = FinetuneModel(exp_cfg,local_rank)
+            ckpt = finetuner.finetune()
+            print("-------------------------")
+            print(ckpt)
+            print("-------------------------")
+
+            wandb.init(
+                config=dict(self.task_cfg.model.inference),
+                project=self.task_cfg.model.logging.wandb_project, name=test_name)
+
+            exp_cfg.model.inference.path.test_data = test_data_path
+            exp_cfg.model.inference.path.pretrained_checkpoint = ckpt
+
+            try:
+                predict = Inference(exp_cfg)
+                predictions = predict.predict()
+                
+            except Exception as e:
+                print(f"Error occurred during inference for finetuned checkpoint '{exp_name}':")
+                print(traceback.format_exc())
+
+        if not os.path.exists(self.benchmark_save_path):
+            os.makedirs(self.benchmark_save_path)
+        import numpy as np
+        np.save(self.benchmark_save_path, predictions.predictions)
