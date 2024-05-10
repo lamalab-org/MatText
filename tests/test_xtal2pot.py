40dab5ddb814eba92a36200d9211427110bc60c6
Kevin Maik Jablonka <mail@kjablonka.com>
2024-05-10 08:33:59 +0200

    added basic test

---
+++ tests/test_xtal2pot.py
@@ -0,0 +1,9 @@
+from structllm.analysis.xtal2pot import Xtal2Pot
+from pymatgen.core import Structure, Lattice
+
+
+def test_xtal2pot():
+    bcc_fe = Structure(Lattice.cubic(2.8), ["Fe", "Fe"], [[0, 0, 0], [0.5, 0.5, 0.5]])
+    xtal2pot = Xtal2Pot()
+    potential = xtal2pot.get_potential(bcc_fe)
+    assert isinstance(potential, float)
