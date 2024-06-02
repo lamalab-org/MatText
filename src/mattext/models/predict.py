d58c23282a0b76e2f52f127ede442a1b0abea1f9
n0w0f <pvt.nawaf@gmail.com>
2024-06-02 02:34:32 +0200

    chore: cleanup and lint

--- src/mattext/models/predict.py
+++ src/mattext/models/predict.py
@@ -13,17 +13,21 @@ from mattext.models.utils import CustomWandbCallback_Inference, TokenizerMixin
 class Inference(TokenizerMixin):
     """Class to perform inference on a language model with a sequence classification head."""
 
-    def __init__(self, cfg: DictConfig):
+    def __init__(self, cfg: DictConfig,fold="fold_0"):
 
         super().__init__(
             cfg=cfg.model.representation,
             special_tokens=cfg.model.special_tokens,
             special_num_token=cfg.model.special_num_token,
         )
+        self.fold = fold
         self.representation = cfg.model.representation
+        self.data_repository = cfg.model.data_repository
+        self.dataset_name = cfg.model.finetune.dataset_name
         self.cfg = cfg.model.inference
         self.context_length: int = self.cfg.context_length
         self.tokenized_test_datasets = self._prepare_datasets(self.cfg.path.test_data)
+        self.prediction_ids = None
 
     def _prepare_datasets(self, path: str) -> DatasetDict:
         """
@@ -35,10 +39,12 @@ class Inference(TokenizerMixin):
         Returns:
             DatasetDict: Dictionary containing training and validation datasets.
         """
-        dataset = load_dataset("json", data_files=path)
-        filtered_dataset = dataset.filter(
+        dataset = load_dataset(self.data_repository, path )
+        filtered_dataset = dataset[self.fold].filter(
             lambda example: example[self.representation] is not None
         )
+        print(filtered_dataset)
+
         return filtered_dataset.map(
             partial(
                 self._tokenize_pad_and_truncate, context_length=self.context_length
@@ -62,7 +68,7 @@ class Inference(TokenizerMixin):
             model=model.to("cuda"), data_collator=None, callbacks=callbacks
         )
 
-        predictions = trainer.predict(self.tokenized_test_datasets["train"])
+        predictions = trainer.predict(self.tokenized_test_datasets)
         for callback in callbacks:
             callback.on_predict_end(
                 None, None, None, model, predictions
@@ -73,5 +79,7 @@ class Inference(TokenizerMixin):
         # os.makedirs(self.cfg.path.predictions, exist_ok=True)
         # predictions_path = os.path.join(self.cfg.path.predictions, 'predictions.npy')
         # np.save(predictions_path, predictions.predictions)
+        preidction_ids = self.tokenized_test_datasets["mbid"]
+        self.prediction_ids = preidction_ids
 
-        return pd.Series(predictions.predictions.flatten())
+        return pd.Series(predictions.predictions.flatten()),preidction_ids
--- src/mattext/representations/decoder.py
+++ src/mattext/representations/decoder.py
@@ -1,4 +1,3 @@
 from xtal2txt.decoder import DecodeTextRep, MatchRep
 
-
 __all__ = ["DecodeTextRep", "MatchRep"]
