16c98fbfeb55fb4a7547f05bdca654424edfc5dc
n0w0f <pvt.nawaf@gmail.com>
2024-06-04 03:40:51 +0200

    llm generation code

--- src/mattext/models/llama_sft.py
+++ src/mattext/models/llama_sft.py
@@ -1,15 +1,9 @@
-import gc
 import json
-from statistics import mean
 from typing import List
 
 import torch
 import wandb
-
-# from evaluate import load
-from accelerate import PartialState
-from datasets import Dataset, DatasetDict, load_dataset
-from dotenv import load_dotenv
+from datasets import load_dataset
 from omegaconf import DictConfig
 from peft import (
     AutoPeftModelForCausalLM,
@@ -34,17 +28,6 @@ from mattext.models.utils import (
     smart_tokenizer_and_embedding_resize,
 )
 
-# Model
-base_model = "meta-llama/Meta-Llama-3-8B-Instruct"
-# Name of the new model
-new_model = "TunedLlama-3-8B"
-exp_name = "test_clm_kvrh_full"
-
-# Dataset
-root = "/work/so87pot/material_db/all_1/"
-# dataset_path = f"{root}train_matbench_log_kvrh_1.json"
-# test_response = f"{root}test_matbench_log_kvrh_1.json"
-
 
 class FinetuneLLamaSFT:
     """Class to perform finetuning of a language model.
@@ -55,9 +38,11 @@ class FinetuneLLamaSFT:
         local_rank (int, optional): Local rank for distributed training. Defaults to None.
     """
 
-    def __init__(self, cfg: DictConfig, local_rank=None) -> None:
+    def __init__(self, cfg: DictConfig, local_rank=None, fold="fold_0") -> None:
+        self.fold = fold
         self.local_rank = local_rank
         self.representation = cfg.model.representation
+        self.data_repository = cfg.model.data_repository
         self.add_special_tokens = cfg.model.add_special_tokens
         self.property_map = cfg.model.PROPERTY_MAP
         self.material_map = cfg.model.MATERIAL_MAP
@@ -69,59 +54,71 @@ class FinetuneLLamaSFT:
         self.model, self.tokenizer, self.peft_config = self._setup_model_tokenizer()
         self.property_ = self.property_map[self.cfg.dataset_name]
         self.material_ = self.material_map[self.cfg.dataset_name]
-        self.dataset = self.prepare_data(
-            self.cfg.path.finetune_traindata
-        )
+        self.dataset = self.prepare_data(self.cfg.path.finetune_traindata)
 
-    def prepare_data(self,dataset_path):
-        dataset = load_dataset("json", data_files=dataset_path, split="train")
-        dataset = dataset.shuffle(seed=42)#.select(range(100))
+    def prepare_data(self, subset):
+        dataset = load_dataset(self.data_repository, subset)
+        dataset = dataset.shuffle(seed=42)  # .select(range(100))
         return dataset.train_test_split(test_size=0.1, seed=42)
 
-
     def _setup_model_tokenizer(self) -> None:
-
         # device_string = PartialState().process_index
         # compute_dtype = getattr(torch, "float16")
 
-        bnb_config = BitsAndBytesConfig(
-            load_in_4bit=True,
-            bnb_4bit_quant_type="nf4",
-            bnb_4bit_use_double_quant=True,
-            bnb_4bit_compute_dtype=torch.bfloat16
-        )
+        if self.bnb_config.use_4bit and self.bnb_config.use_8bit:
+            raise ValueError(
+                "You can't load the model in 8 bits and 4 bits at the same time"
+            )
 
-            # LoRA config
-        peft_config = LoraConfig(
-            r=32, # The rank of the update matrices, expressed in int. Lower rank results in smaller update matrices with fewer trainable parameters.
-            lora_alpha=64, # LoRA scaling factor. It changes how the adaptation layer's weights affect the base model's
-            lora_dropout=0.1, # Dropout is a regularization technique where a proportion of neurons (or parameters) are randomly “dropped out” or turned off during training to prevent overfitting.
-            bias="none", # Specifies if the bias parameters should be trained. Can be 'none', 'all' or 'lora_only'.
-            task_type="CAUSAL_LM", # Task to perform, Causal LM: Causal language modeling.
+        elif self.bnb_config.use_4bit or self.bnb_config.use_8bit:
+            compute_dtype = getattr(torch, self.bnb_config.bnb_4bit_compute_dtype)
+            bnb_config = BitsAndBytesConfig(
+                load_in_4bit=self.bnb_config.use_4bit,
+                load_in_8bit=self.bnb_config.use_8bit,
+                bnb_4bit_quant_type=self.bnb_config.bnb_4bit_quant_type,
+                bnb_4bit_compute_dtype=compute_dtype,
+                bnb_4bit_use_double_quant=self.bnb_config.use_nested_quant,
+            )
+        else:
+            bnb_config = None
+
+        # Check GPU compatibility with bfloat16
+        if compute_dtype == torch.float16:
+            major, _ = torch.cuda.get_device_capability()
+            if major >= 8:
+                print("=" * 80)
+                print("Your GPU supports bfloat16: accelerate training with bf16=True")
+                print("=" * 80)
+
+        # LoRA config
+        peft_config = LoraConfig(**self.cfg.lora_config)
+
+        tokenizer = AutoTokenizer.from_pretrained(
+            self.ckpt,
+            use_fast=False,
         )
-        tokenizer = AutoTokenizer.from_pretrained(base_model,use_fast=False,)
         tokenizer.pad_token = tokenizer.eos_token
         tokenizer.padding_side = "right"
 
         model = AutoModelForCausalLM.from_pretrained(
-                base_model,
-                quantization_config=bnb_config,
-                device_map="auto",
-            )
+            self.ckpt,
+            quantization_config=bnb_config,
+            device_map="auto",
+        )
 
         return model, tokenizer, peft_config
 
-    def formatting_prompts_func(self,example):
+    def formatting_prompts_func(self, example):
         output_texts = []
         for i in range(len(example[self.representation])):
-            text = f"### What is the property of {example[self.representation][i]}\n ### Answer: {example['labels'][i]:.3f}@@@"
+            text = f"### What is the {self.property_} of {example[self.representation][i]}\n ### Answer: {example['labels'][i]:.3f}@@@"
             output_texts.append(text)
         return output_texts
 
-    def formatting_tests_func(self,example):
+    def formatting_tests_func(self, example):
         output_texts = []
         for i in range(len(example[self.representation])):
-            text = f"### What is the property of {example[self.representation][i]}\n "
+            text = f"### What is the {self.property_} of {example[self.representation][i]}\n "
             output_texts.append(text)
         return output_texts
 
@@ -151,19 +148,20 @@ class FinetuneLLamaSFT:
         callbacks = self._callbacks()
 
         response_template = " ### Answer:"
-        collator = DataCollatorForCompletionOnlyLM(response_template, tokenizer=self.tokenizer)
+        collator = DataCollatorForCompletionOnlyLM(
+            response_template, tokenizer=self.tokenizer
+        )
 
         packing = False
         max_seq_length = None
         if self.representation == "cif_p1":
             max_seq_length = 2048
 
-
         trainer = SFTTrainer(
             model=self.model,
             peft_config=self.peft_config,
-            train_dataset=self.dataset['train'],
-            eval_dataset=self.dataset['test'],
+            train_dataset=self.dataset["train"],
+            eval_dataset=self.dataset["test"],
             formatting_func=self.formatting_prompts_func,
             data_collator=collator,
             max_seq_length=max_seq_length,
@@ -186,13 +184,15 @@ class FinetuneLLamaSFT:
         )
         trainer.train()
 
-        testset = load_dataset("json", data_files=self.cfg.path.finetune_testdata, split="train")
+        testset = load_dataset(
+            "json", data_files=self.cfg.path.finetune_testdata, split="train"
+        )
 
         pipe = pipeline(
             "text-generation",
             model=trainer.model,
             tokenizer=self.tokenizer,
-            return_full_text = False,
+            return_full_text=False,
             do_sample=False,
             max_new_tokens=4,
         )
@@ -200,32 +200,35 @@ class FinetuneLLamaSFT:
             pred = pipe(self.formatting_tests_func(testset))
         print(pred)
 
-        with open(f"{self.cfg.path.finetuned_modelname}_predictions.json", 'w') as json_file:
+        with open(
+            f"{self.cfg.path.finetuned_modelname}_{self.fold}_predictions.json", "w"
+        ) as json_file:
             json.dump(pred, json_file)
-
 
         trainer.save_state()
         trainer.save_model(self.output_dir_)
 
-       # Merge LoRA and base model
+        # Merge LoRA and base model
         merged_model = trainer.model.merge_and_unload()
         # Save the merged model
         merged_model.save_pretrained(
-            f"{self.cfg.path.finetuned_modelname}/llamav3-8b-lora-save-pretrained",
+            f"{self.cfg.path.finetuned_modelname}_{self.fold}/llamav3-8b-lora-save-pretrained",
             save_config=True,
             safe_serialization=True,
         )
         self.tokenizer.save_pretrained(
-            f"{self.cfg.path.finetuned_modelname}/llamav3-8b-lora-save-pretrained"
+            f"{self.cfg.path.finetuned_modelname}_{self.fold}/llamav3-8b-lora-save-pretrained"
         )
 
         with torch.cuda.amp.autocast():
             merge_pred = pipe(self.formatting_tests_func(testset))
         print(merge_pred)
 
-        with open(f"{self.cfg.path.finetuned_modelname}_predictions_merged.json", 'w') as json_file:
+        with open(
+            f"{self.cfg.path.finetuned_modelname}__{self.fold}_predictions_merged.json",
+            "w",
+        ) as json_file:
             json.dump(merge_pred, json_file)
 
         wandb.finish()
         return self.cfg.path.finetuned_modelname
-
