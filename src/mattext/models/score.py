824471adb1c25fc5903739576146fb60f033bf1a
n0w0f <pvt.nawaf@gmail.com>
2024-08-20 14:42:56 +0200

    chore: refactor task

--- src/mattext/models/score.py
+++ src/mattext/models/score.py
@@ -1,8 +1,8 @@
-import json
 import math
-from dataclasses import asdict, dataclass, field
+from dataclasses import dataclass, field
 from typing import Any, Dict, List
 
+import jsonpickle
 import numpy as np
 import pandas as pd
 from matbench.data_ops import load
@@ -13,13 +13,12 @@ from sklearn.metrics import (
     precision_recall_fscore_support,
     roc_auc_score,
 )
-from sklearn.preprocessing import label_binarize
 
 MATTEXT_MATBENCH = {
     "kvrh": "matbench_log_kvrh",
     "gvrh": "matbench_log_gvrh",
     "perovskites": "matbench_perovskites",
-    "bandgap" : "matbench_mp_gap",
+    "bandgap": "matbench_mp_gap",
     "form_energy": "matbench_mp_e_form",
     "is-metal": "matbench_mp_is_metal",
 }
@@ -33,37 +32,20 @@ MATMINER_COLUMNS = {
     "form_energy": "e_form",
 }
 
-METRIC_MAP = {
-    "mae": mean_absolute_error,
-    "rmse": lambda true, pred: math.sqrt(mean_squared_error(true, pred)),
-}
-
-
-def fold_key_namer(fold_key):
-    return f"fold_{fold_key}"
-
 
 def load_true_scores(dataset, mbids):
     data_frame = load(MATTEXT_MATBENCH[dataset])
-    print(MATMINER_COLUMNS)
     scores = []
     for mbid in mbids:
-        # Get the score for the mbid
         score = data_frame.loc[mbid][MATMINER_COLUMNS[dataset]]
         scores.append(score)
     return scores
 
 
-def mattext_score(prediction_ids, predictions, task_name):
-    true = load_true_scores(task_name, prediction_ids)
-    return mean_squared_error(true, predictions)
-
-
 @dataclass
-class MatTextTask:
+class BaseMatTextTask:
     task_name: str
     num_folds: int = 5
-    # metric: str
     folds_results: Dict[int, Dict[str, Any]] = field(default_factory=dict)
     recorded_folds: List[int] = field(default_factory=list)
 
@@ -73,6 +55,35 @@ class MatTextTask:
         if fold in self.recorded_folds:
             raise ValueError(f"Fold {fold} has already been recorded.")
         true_scores = load_true_scores(self.task_name, prediction_ids)
+        self._calculate_metrics(fold, prediction_ids, predictions, true_scores)
+        self.recorded_folds.append(fold)
+
+    def _calculate_metrics(self, fold, prediction_ids, predictions, true_scores):
+        raise NotImplementedError("Subclasses must implement this method")
+
+    def get_final_results(self):
+        if len(self.recorded_folds) < self.num_folds:
+            raise ValueError(
+                f"All {self.num_folds} folds must be recorded before getting final results."
+            )
+        return self._aggregate_results()
+
+    def _aggregate_results(self):
+        raise NotImplementedError("Subclasses must implement this method")
+
+    def to_file(self, file_path: str):
+        with open(file_path, "w") as f:
+            f.write(jsonpickle.encode(self))
+
+    @staticmethod
+    def from_file(file_path: str):
+        with open(file_path) as f:
+            return jsonpickle.decode(f.read())
+
+
+@dataclass
+class MatTextTask(BaseMatTextTask):
+    def _calculate_metrics(self, fold, prediction_ids, predictions, true_scores):
         mae = mean_absolute_error(true_scores, predictions)
         rmse = math.sqrt(mean_squared_error(true_scores, predictions))
         self.folds_results[fold] = {
@@ -82,91 +93,33 @@ class MatTextTask:
             "mae": mae,
             "rmse": rmse,
         }
-        self.recorded_folds.append(fold)
 
-    def get_final_results(self):
-        if len(self.recorded_folds) < self.num_folds:
-            raise ValueError(
-                f"All {self.num_folds} folds must be recorded before getting final results."
-            )
+    def _aggregate_results(self):
         final_scores_mae = [
             self.folds_results[fold]["mae"] for fold in range(self.num_folds)
         ]
         final_scores_rmse = [
             self.folds_results[fold]["rmse"] for fold in range(self.num_folds)
         ]
-
         return {
             "mean_mae_score": np.mean(final_scores_mae),
             "std_mae_score": np.std(final_scores_mae),
             "mean_rmse_score": np.mean(final_scores_rmse),
             "std_rmse_score": np.std(final_scores_rmse),
-            "std_score": np.std(final_scores_mae),
         }
-
-    def to_file(self, file_path: str):
-        final_results = (
-            self.get_final_results()
-            if len(self.recorded_folds) == self.num_folds
-            else {}
-        )
-        data_to_save = asdict(self)
-        data_to_save["final_results"] = final_results
-        with open(file_path, "w") as f:
-            json.dump(data_to_save, f, default=self._json_serializable)
-
-    @staticmethod
-    def from_file(file_path: str):
-        with open(file_path) as f:
-            data = json.load(f)
-        task = MatTextTask(task_name=data["task_name"], metric=data["metric"])
-        task.folds_results = data["folds_results"]
-        task.recorded_folds = data["recorded_folds"]
-        return task
-
-    @staticmethod
-    def _prepare_for_serialization(obj):
-        if isinstance(obj, dict):
-            return {
-                k: MatTextTask._prepare_for_serialization(v) for k, v in obj.items()
-            }
-        elif (
-            isinstance(obj, (list, pd.Series, np.ndarray))
-        ):
-            return MatTextTask._prepare_for_serialization(obj.tolist())
-        else:
-            return obj
-
-    @staticmethod
-    def _json_serializable(obj):
-        if isinstance(obj, (np.ndarray, pd.Series)):
-            return obj.tolist()
-        raise TypeError(f"Type {type(obj)} not serializable")
-
-
 
 
 @dataclass
-class MatTextClassificationTask:
-    task_name: str
-    num_folds: int = 5
+class MatTextClassificationTask(BaseMatTextTask):
     num_classes: int = 2
-    folds_results: Dict[int, Dict[str, Any]] = field(default_factory=dict)
-    recorded_folds: List[int] = field(default_factory=list)
 
-    def record_fold(
-        self, fold: int, prediction_ids: List[str], predictions: List[float]
-    ):
-        if fold in self.recorded_folds:
-            raise ValueError(f"Fold {fold} has already been recorded.")
-
-        true_labels = load_true_scores(self.task_name, prediction_ids)
+    def _calculate_metrics(self, fold, prediction_ids, predictions, true_labels):
         pred_labels = np.argmax(predictions, axis=1)
-
         accuracy = accuracy_score(true_labels, pred_labels)
-        precision, recall, f1, _ = precision_recall_fscore_support(true_labels, pred_labels, average='weighted')
+        precision, recall, f1, _ = precision_recall_fscore_support(
+            true_labels, pred_labels, average="weighted"
+        )
         roc_auc = roc_auc_score(true_labels, predictions[:, 1])
-
         self.folds_results[fold] = {
             "prediction_ids": prediction_ids,
             "predictions": predictions,
@@ -175,56 +128,15 @@ class MatTextClassificationTask:
             "precision": precision,
             "recall": recall,
             "f1": f1,
-            "roc_auc": roc_auc
+            "roc_auc": roc_auc,
         }
-        self.recorded_folds.append(fold)
 
-    def get_final_results(self):
-        if len(self.recorded_folds) < self.num_folds:
-            raise ValueError(
-                f"All {self.num_folds} folds must be recorded before getting final results."
-            )
-        metrics = ['accuracy', 'precision', 'recall', 'f1', 'roc_auc']
+    def _aggregate_results(self):
+        metrics = ["accuracy", "precision", "recall", "f1", "roc_auc"]
         final_scores = {metric: [] for metric in metrics}
-
         for fold in range(self.num_folds):
             for metric in metrics:
                 final_scores[metric].append(self.folds_results[fold][metric])
-
         return {
             f"mean_{metric}": np.mean(scores) for metric, scores in final_scores.items()
-        } | {
-            f"std_{metric}": np.std(scores) for metric, scores in final_scores.items()
-        }
-
-    def to_file(self, file_path: str):
-        final_results = (
-            self.get_final_results()
-            if len(self.recorded_folds) == self.num_folds
-            else {}
-        )
-        data_to_save = asdict(self)
-        data_to_save["final_results"] = final_results
-        with open(file_path, "w") as f:
-            json.dump(data_to_save, f, default=self._json_serializable)
-
-    @staticmethod
-    def from_file(file_path: str):
-        with open(file_path) as f:
-            data = json.load(f)
-        task = MatTextClassificationTask(task_name=data["task_name"], num_classes=data["num_classes"])
-        task.folds_results = data["folds_results"]
-        task.recorded_folds = data["recorded_folds"]
-        return task
-
-    @staticmethod
-    def _json_serializable(obj):
-        if isinstance(obj, (np.ndarray, pd.Series)):
-            return obj.tolist()
-        elif isinstance(obj, np.bool_):
-            return bool(obj)
-        elif isinstance(obj, np.integer):
-            return int(obj)
-        elif isinstance(obj, np.floating):
-            return float(obj)
-        raise TypeError(f"Type {type(obj)} not serializable")
+        } | {f"std_{metric}": np.std(scores) for metric, scores in final_scores.items()}
