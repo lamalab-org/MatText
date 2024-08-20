a55281fe292a810a5a144f35cc196b2f12f36a44
n0w0f <pvt.nawaf@gmail.com>
2024-08-20 14:33:44 +0200

    chore: abstract out inference from reg and classification to base

--- src/mattext/models/predict.py
+++ src/mattext/models/predict.py
@@ -1,5 +1,6 @@
+from abc import ABC, abstractmethod
 from functools import partial
-from typing import List, Tuple
+from typing import List, Tuple, Union
 
 import numpy as np
 import pandas as pd
@@ -17,9 +18,7 @@ from transformers import AutoModelForSequenceClassification, Trainer, TrainerCal
 from mattext.models.utils import CustomWandbCallback_Inference, TokenizerMixin
 
 
-class Inference(TokenizerMixin):
-    """Class to perform inference on a language model with a sequence classification head."""
-
+class BaseInference(TokenizerMixin, ABC):
     def __init__(self, cfg: DictConfig, fold="fold_0"):
         super().__init__(
             cfg=cfg.model.representation,
@@ -36,20 +35,10 @@ class Inference(TokenizerMixin):
         self.prediction_ids = None
 
     def _prepare_datasets(self, path: str) -> DatasetDict:
-        """
-        Prepare training and validation datasets.
-
-        Args:
-            train_df (pd.DataFrame): DataFrame containing training data.
-
-        Returns:
-            DatasetDict: Dictionary containing training and validation datasets.
-        """
         dataset = load_dataset(self.data_repository, path)
         filtered_dataset = dataset[self.fold].filter(
             lambda example: example[self.representation] is not None
         )
-
         return filtered_dataset.map(
             partial(
                 self._tokenize_pad_and_truncate, context_length=self.context_length
@@ -58,136 +47,68 @@ class Inference(TokenizerMixin):
         )
 
     def _callbacks(self) -> List[TrainerCallback]:
-        """Returns a list of callbacks for logging."""
         return [CustomWandbCallback_Inference()]
 
-    def predict(self):
+    @abstractmethod
+    def get_model(self, pretrained_ckpt: str):
+        pass
+
+    @abstractmethod
+    def process_predictions(self, predictions) -> Union[pd.Series, pd.DataFrame]:
+        pass
+
+    def predict(self) -> Tuple[Union[pd.Series, pd.DataFrame], List[str]]:
         pretrained_ckpt = self.cfg.path.pretrained_checkpoint
         callbacks = self._callbacks()
 
-        model = AutoModelForSequenceClassification.from_pretrained(
+        model = self.get_model(pretrained_ckpt)
+
+        trainer = Trainer(
+            model=model.to("cuda"), data_collator=None, callbacks=callbacks
+        )
+
+        predictions = trainer.predict(self.tokenized_test_datasets)
+        for callback in callbacks:
+            callback.on_predict_end(None, None, None, model, predictions)
+        torch.cuda.empty_cache()
+
+        prediction_ids = self.tokenized_test_datasets["mbid"]
+        self.prediction_ids = prediction_ids
+
+        processed_predictions = self.process_predictions(predictions)
+
+        return processed_predictions, prediction_ids
+
+
+class Inference(BaseInference):
+    def get_model(self, pretrained_ckpt: str):
+        return AutoModelForSequenceClassification.from_pretrained(
             pretrained_ckpt, num_labels=1, ignore_mismatched_sizes=False
         )
 
-        trainer = Trainer(
-            model=model.to("cuda"), data_collator=None, callbacks=callbacks
-        )
-
-        predictions = trainer.predict(self.tokenized_test_datasets)
-        for callback in callbacks:
-            callback.on_predict_end(
-                None, None, None, model, predictions
-            )  # Manually trigger callback
-        torch.cuda.empty_cache()
-
-        # TODO: Save predictions to disk optional
-        # os.makedirs(self.cfg.path.predictions, exist_ok=True)
-        # predictions_path = os.path.join(self.cfg.path.predictions, 'predictions.npy')
-        # np.save(predictions_path, predictions.predictions)
-        prediction_ids = self.tokenized_test_datasets["mbid"]
-        self.prediction_ids = prediction_ids
-
-        return pd.Series(predictions.predictions.flatten()), prediction_ids
+    def process_predictions(self, predictions) -> pd.Series:
+        return pd.Series(predictions.predictions.flatten())
 
 
-class InferenceClassification(TokenizerMixin):
-    """Class to perform inference on a language model with a sequence classification head for classification tasks."""
-
+class InferenceClassification(BaseInference):
     def __init__(self, cfg: DictConfig, fold="fold_0"):
-        super().__init__(
-            cfg=cfg.model.representation,
-            special_tokens=cfg.model.special_tokens,
-            special_num_token=cfg.model.special_num_token,
-        )
-        self.fold = fold
-        self.representation = cfg.model.representation
-        self.data_repository = cfg.model.data_repository
-        self.dataset_name = cfg.model.finetune.dataset_name
-        self.cfg = cfg.model.inference
-        self.context_length: int = self.cfg.context_length
-        #self.num_labels = cfg.model.num_labels
-        self.num_labels = 2
-        self.tokenized_test_datasets = self._prepare_datasets(self.cfg.path.test_data)
-        self.prediction_ids = None
+        super().__init__(cfg, fold)
+        self.num_labels = 2  # You might want to make this configurable
 
-    def _prepare_datasets(self, path: str) -> DatasetDict:
-        """
-        Prepare test datasets.
-
-        Args:
-            path (str): Path to the test data.
-
-        Returns:
-            DatasetDict: Dictionary containing the test dataset.
-        """
-        dataset = load_dataset(self.data_repository, path)
-        filtered_dataset = dataset[self.fold].filter(
-            lambda example: example[self.representation] is not None
-        )
-
-        return filtered_dataset.map(
-            partial(
-                self._tokenize_pad_and_truncate, context_length=self.context_length
-            ),
-            batched=True,
-        )
-
-    def _callbacks(self) -> List[TrainerCallback]:
-        """Returns a list of callbacks for logging."""
-        return [CustomWandbCallback_Inference()]
-
-    def predict(self) -> Tuple[pd.DataFrame, List[str]]:
-        """
-        Perform prediction on the test dataset.
-
-        Returns:
-            Tuple[pd.DataFrame, List[str]]: A tuple containing the predictions as a DataFrame
-            and the prediction IDs as a list.
-        """
-        pretrained_ckpt = self.cfg.path.pretrained_checkpoint
-        callbacks = self._callbacks()
-
-        model = AutoModelForSequenceClassification.from_pretrained(
+    def get_model(self, pretrained_ckpt: str):
+        return AutoModelForSequenceClassification.from_pretrained(
             pretrained_ckpt, num_labels=self.num_labels, ignore_mismatched_sizes=False
         )
 
-        trainer = Trainer(
-            model=model.to("cuda"), data_collator=None, callbacks=callbacks
-        )
-
-        predictions = trainer.predict(self.tokenized_test_datasets)
-        for callback in callbacks:
-            callback.on_predict_end(
-                None, None, None, model, predictions
-            )  # Manually trigger callback
-        torch.cuda.empty_cache()
-
-        prediction_ids = self.tokenized_test_datasets["mbid"]
-        self.prediction_ids = prediction_ids
-
-        # Convert predictions to probabilities
+    def process_predictions(self, predictions) -> pd.DataFrame:
         probabilities = torch.nn.functional.softmax(
             torch.from_numpy(predictions.predictions), dim=-1
         ).numpy()
-
-        #Create a DataFrame with prediction probabilities
-        prediction_df = pd.DataFrame(
+        return pd.DataFrame(
             probabilities, columns=[f"class_{i}" for i in range(self.num_labels)]
         )
 
-        return prediction_df, prediction_ids
-
     def evaluate(self, true_labels: List[int]) -> dict:
-        """
-        Evaluate the model's predictions against true labels.
-
-        Args:
-            true_labels (List[int]): The true labels for the test set.
-
-        Returns:
-            dict: A dictionary containing evaluation metrics.
-        """
-
         predictions, _ = self.predict()
         pred_labels = np.argmax(predictions.values, axis=1)
 
